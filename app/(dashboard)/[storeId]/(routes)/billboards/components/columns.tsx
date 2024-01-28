86b15e182c5ab2f42a3319a123db433f1c81a98b
Matheus Henrique <matheus.mhsantos@mercadolivre.com>
2024-01-28 10:26:22 -0300

    Billboards Add Data Table

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table"
+
+import { CellAction } from "./cell-action";
+
+export type BillboardColumn = {
+  id: string
+  label: string
+  created_at: string
+}
+
+export const columns: ColumnDef<BillboardColumn>[] = [
+    {
+        accessorKey: "id",
+        header: "Billboard ID",
+    },
+    {
+        accessorKey: "label",
+        header: "Label",
+    },
+    {
+        accessorKey: "created_at",
+        header: "Date",
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => <CellAction data={row.original} />
+    },
+]
